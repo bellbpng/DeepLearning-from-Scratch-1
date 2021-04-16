8a6fab1a0e47477f781c08af0e79b2aff78df68d
Bokyoung <bellbpng@gmail.com>
2021-04-16 13:50:18 +0900

    update numerical-differentation

--- Ch4/numerical_differentation.py
+++ Ch4/numerical_differentation.py
@@ -11,18 +11,37 @@ def numerical_diff(f,x):
 def function_1(x):
     return 0.01*x**2+0.1*x
 
-x = np.arange(0.0, 20.0, 0.1) #0에서 20까지 0.1간격으로 나눈 배열을 형성
+def tangent_line(f, x):
+    d = numerical_diff(f, x)
+    print(d)
+    y = f(x) - d*x
+    return lambda t: d*t + y
+'''
+사용된 람다 표현식 정리 (lambda)
+- plus_ten = lambda x: x+10
+=> 매개변수 x를 전달받아 10을 더한 값을 반환하는 익명함수로 활용
+=> plus_ten(20), plus_ten(11) 와 같이 람다 표현식이 할당된 변수에 매개변수를 전달해주는 형태로 호출
+
+'''
+x = np.arange(0.0, 20.0, 0.1)
 y = function_1(x)
 plt.xlabel("x")
 plt.ylabel("f(x)")
-plt.plot(x,y)
+
+tf = tangent_line(function_1, 5)
+y2 = tf(x) # 람다식에 매개변수를 전달
+
+plt.plot(x, y)
+plt.plot(x, y2)
 plt.show()
 
+
 #기울기 - 모든 변수의 편미분을 벡터로 정리한 것
-def numerical_gardient(f,x):
+def _numerical_gradient_no_batch(f,x):
     h = 1e-4 #0.0001
     grad = np.zeros_like(x) #x와 형상이 같고 그 원소가 모두 0인 배열을 만듦
     
+    # x의 각 원소에 대해서 수치미분을 구한다.
     for idx in range(x.size):
         tmp_val = x[idx]
         x[idx] = tmp_val + h
@@ -35,4 +54,49 @@ def numerical_gardient(f,x):
         x[idx] = tmp_val
         
     return grad
+
+def numerical_gradient(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_no_batch(f, X)
+    else:
+        grad = np.zeros_like(X)
+        
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_no_batch(f, x)
+        
+        return grad
+
+def function_2(x):
+    if x.ndim == 1:
+        return np.sum(x**2)
+    else:
+        return np.sum(x**2, axis=1)
+
+
+def tangent_line(f, x):
+    d = numerical_gradient(f, x)
+    print(d)
+    y = f(x) - d*x
+    return lambda t: d*t + y
     
+if __name__ == '__main__':
+    x0 = np.arange(-2, 2.5, 0.25)
+    x1 = np.arange(-2, 2.5, 0.25)
+    X, Y = np.meshgrid(x0, x1)
+    
+    X = X.flatten()
+    Y = Y.flatten()
+    
+    grad = numerical_gradient(function_2, np.array([X, Y]) )
+    
+    plt.figure()
+    plt.quiver(X, Y, -grad[0], -grad[1],  angles="xy",color="#666666")#,headwidth=10,scale=40,color="#444444")
+    plt.xlim([-2, 2])
+    plt.ylim([-2, 2])
+    plt.xlabel('x0')
+    plt.ylabel('x1')
+    plt.grid()
+    plt.legend()
+    plt.draw()
+    plt.show()
+
