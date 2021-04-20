869cc5963e099538d4ed093e4fede09da1ab48ec
Bokyoung <bellbpng@gmail.com>
2021-04-20 13:29:01 +0900

    update two-layer-network

--- Ch4/TwoLayerNet.py
+++ Ch4/TwoLayerNet.py
@@ -8,7 +8,7 @@ class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):        
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
-        self.params['b1'] = np.zeors(hidden_size)
+        self.params['b1'] = np.zeros(hidden_size) # 0으로 가득찬 hidden_size 크기의 1차원 배열을 형성
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
 
@@ -26,7 +26,7 @@ class TwoLayerNet:
 
     # 손실함수 값 계산. x: 입력데이터, t: 정답레이블
     def loss(self, x, t):
-        y = self.predict(self, x)
+        y = self.predict(x)
 
         return cross_entropy_error(y, t)
 
@@ -50,4 +50,41 @@ class TwoLayerNet:
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
         
         return grads
+
+    # 오차역전파법. 기울기 계산을 고속으로 수행하는 방법
+    def gradient(self, x, t):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
         
+        batch_num = x.shape[0]
+        
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+        
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+        
+        da1 = np.dot(dy, W2.T)
+        dz1 = sigmoid_grad(a1) * da1
+        grads['W1'] = np.dot(x.T, dz1)
+        grads['b1'] = np.sum(dz1, axis=0)
+
+        return grads
+
+net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
+print("가중치와 매개변수의 형상")
+print(net.params['W1'].shape)
+print(net.params['b1'].shape)
+print(net.params['W2'].shape)
+print(net.params['b2'].shape)
+print()
+
+x = np.random.rand(100, 784) # 100장 분량의 더미 입력데이터
+t = np.random.rand(100, 10) # 100장 분량의 더미 정답레이블
+y = net.predict(x)
